2a4df1d5edf26cf0ae4deff18b7a5a88d01a3a1e
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-06-15 15:11:10 -0600

    Site can be hosted locally, and error codes are sent for non-existent pages

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -8,3 +8,12 @@ class HomepageTest < CapybaraTestCase
     assert_equal 200, page.status_code
   end
 end
+
+class ErrorTest < CapybaraTestCase
+  def test_user_sees_error_page_when_appropriate
+    visit '/account'
+
+    assert_equal 404, page.status_code
+    assert page.has_content?("Page not found.")
+  end
+end
